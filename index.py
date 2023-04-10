ef2e66122de91fa1a39a2b5e817cc05284b6dd5d
Ari <88184960+Templight41@users.noreply.github.com>
2023-04-10 21:33:55 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,11 +1,11 @@
-from flask import Flask,render_template
+from flask import Flask
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route("/")
+@app.route('/', methods=['GET'])
 def home():
-    return render_template('index.html')
+    return "Hello world!"
 
 
-if __name__ == "__main__":
-    app.run()#(debug=False,host='0.0.0.0')
+if (__name__ == '__main__'):
+    app.run()
